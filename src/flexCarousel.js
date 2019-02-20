c674049a780e560aabbf89110655a09ba067b55c
Tom Hartley <tom@tomhrtly.com>
2019-02-20 19:14:30 +0000

    Finish rebuild

--- src/flexCarousel.js
+++ src/flexCarousel.js
@@ -35,9 +35,8 @@
         circles: true,
         circlesOverlay: true,
         height: null,
-        loop: true,
-        nextArrow: '<div class="fc-next"><span class="fc-icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" class="svg-inline--fa fa-angle-right fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg></span></div>',
-        prevArrow: '<div class="fc-prev"><span class="fc-icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left" class="svg-inline--fa fa-angle-left fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg></span></div>',
+        nextArrow: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" class="svg-inline--fa fa-angle-right fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg>',
+        prevArrow: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left" class="svg-inline--fa fa-angle-left fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>',
         slidesVisible: 1,
         transition: 'slide',
       };
@@ -52,22 +51,189 @@
     return flexCarousel;
   }());
 
-  flexCarousel.prototype.buildArrows = function() {
+  var object = flexCarousel.prototype;
+
+  object.autoplay = function() {
     var self = this;
 
-    if(self.options.slidesVisible) {
-      console.log('yes');
+    if(self.options.autoplay) {
+      setInterval(function() { self.moveSlide('next'); }, self.options.autoplaySpeed);
     }
   }
 
-  flexCarousel.prototype.buildSlides = function() {
+  object.buildArrowEvents = function() {
     var self = this;
-    var slide = self.selector.find('div').addClass('fc-slide');
+    var prev = self.selector.find('.fc-prev');
+    var next = self.selector.find('.fc-next');
 
-    slide.wrapAll('<div class="fc-container"><div class="fc-slides ' + self.transitionClasses() + '" /></div>');
+    if(self.options.arrows) {
+      if(self.options.arrowsOverlay) {
+        prev.click(function() { self.moveSlide('prev'); });
+        next.click(function() { self.moveSlide('next'); });
+      } else {
+        self.selector.find('.fc-prev .fc-icon').click(function(){ self.moveSlide('prev'); });
+        self.selector.find('.fc-next .fc-icon').click(function(){ self.moveSlide('next'); });
+      }
+    }
   }
 
-  flexCarousel.prototype.height = function() {
+  object.buildArrows = function() {
+    var self = this;
+    var slide = self.selector.find('.fc-slide');
+
+    if(self.options.arrows) {
+      if(self.options.slidesVisible < slide.length) {
+        self.selector.prepend('<div class="fc-prev"><span class="fc-icon">' + self.options.prevArrow + '</span></div>');
+        self.selector.append('<div class="fc-next"><span class="fc-icon">' + self.options.nextArrow + '</span></div>');
+
+        var prev = self.selector.find('.fc-prev');
+        var next = self.selector.find('.fc-next');
+
+        next.addClass('fc-is-active');
+        prev.addClass('fc-is-active');
+
+        if (self.options.arrowsOverlay) {
+          self.selector.addClass('fc-has-overlay');
+        }
+      }
+    }
+  }
+
+  object.buildCircleEvents = function() {
+    var self = this;
+    var circle = self.selector.find('.fc-circle');
+    var slide = self.selector.find('.fc-slide');
+
+    if(self.options.circles) {
+      circle.click(function() {
+        var index = $(this).index();
+
+        $(this).addClass('fc-is-active');
+        circle.not($(this)).removeClass('fc-is-active');
+
+        slide.eq(index).addClass('fc-is-active').siblings().removeClass('fc-is-active');
+      });
+    }
+  }
+
+  object.buildCircles = function() {
+    var self = this;
+    var container = self.selector.find('.fc-container');
+    var slide = self.selector.find('.fc-slide');
+
+    if(self.options.circles) {
+      container.append('<div class="fc-circles" />');
+      var circles = self.selector.find('.fc-circles');
+
+      slide.each(function () {
+        circles.append('<div class="fc-circle"><span class="fc-icon fc-is-circle"></span></div>');
+      });
+      var circle = self.selector.find('.fc-circle');
+
+      circle.first().addClass('fc-is-active');
+
+      if(self.options.circlesOverlay) {
+        container.addClass('fc-has-overlay');
+        circles.addClass('fc-is-overlay');
+      }
+    }
+  }
+
+  object.buildEvents = function() {
+    var self = this;
+
+    self.buildArrowEvents();
+    self.buildCircleEvents();
+  }
+
+  object.buildSlides = function() {
+    var self = this;
+
+    var slide = self.selector.find('div');
+    slide.addClass('fc-slide').wrapAll('<div class="fc-container"><div class="fc-slides ' + self.transitionClasses() + '" /></div>');
+
+    var slideWidth = 100 / self.options.slidesVisible + '%';
+
+    var slides = self.selector.find('.fc-slides');
+
+    slide.first().addClass('fc-is-active');
+
+    if(self.options.slidesVisible < slide.length) {
+      slide.last().css('order', 1);
+
+      var i = 2;
+      slide.slice(0, slide.length - 1).each(function () {
+        $(this).css('order', i++);
+      });
+
+      slide.each(function () {
+        var image = slide.find('img');
+        var imageCaption = image.data('caption');
+
+        // Wrap the images and use data attribute for captions for cleaner HTML markup
+        image.wrap('<figure class="fc-image"></figure>');
+
+        if (imageCaption) {
+          image.after('<figcaption>' + imageCaption + '</figcaption>');
+        }
+
+        $(this).css('min-width', 'calc(100% / ' + self.options.slidesVisible + ')');
+      });
+    }
+
+    if(self.options.slidesVisible >= slide.length) {
+      slides.css('left', 'auto');
+    } else {
+      slides.css('left', '-' + slideWidth);
+    }
+
+    if(self.options.transition === 'slide') {
+      slides.css('transform', 'translateX(' + slideWidth + ')');
+    }
+  }
+
+  object.changeOrder = function(amount) {
+    var self = this;
+    var slides = self.selector.find('.fc-slides');
+    var slide = self.selector.find('.fc-slide');
+    var slideWidth = 100 / self.options.slidesVisible + '%';
+
+    if(amount === 'increase') {
+      slide.each(function() {
+        var convertedOrder = parseInt($(this).css('order'));
+        var orderIncrease = convertedOrder + 1;
+
+        if(convertedOrder === slide.length) {
+          $(this).css('order', '1');
+        } else {
+          $(this).css('order', orderIncrease);
+        }
+      });
+
+      // Determine whether the carousel is going forward or backward
+      if(self.options.transition === 'slide') {
+        slides.css('transform', 'translateX(-' + slideWidth + ')');
+      }
+    } else {
+      slide.each(function() {
+        var convertedOrder = parseInt($(this).css('order'));
+        var orderDecrease = convertedOrder - 1;
+
+        if(convertedOrder === 1) {
+          $(this).css('order', slide.length);
+        } else {
+          $(this).css('order', orderDecrease);
+        }
+      });
+
+      // Determine whether the carousel is going forward or backward
+      if(self.options.transition === 'slide') {
+        slides.css('transform', 'translateX(' + slideWidth + ')');
+      }
+    }
+  }
+
+  object.height = function() {
     var self = this;
 
     if(self.options.height) {
@@ -75,19 +241,46 @@
     }
   }
 
-  flexCarousel.prototype.init = function() {
+  object.init = function() {
     var self = this;
 
     if (!self.selector.hasClass('fc')) {
       self.selector.addClass('fc');
 
-      self.buildArrows();
       self.buildSlides();
+      self.buildArrows();
+      self.buildCircles();
+      self.buildEvents();
       self.height();
+      self.autoplay();
     }
   };
 
-  flexCarousel.prototype.transitionClasses = function() {
+  object.moveSlide = function(direction) {
+    var self = this;
+
+    if(direction) {
+      setTimeout(function () { self.transition(); }, 1);
+      self.transition();
+    }
+
+    if(direction === 'next') {
+      self.changeOrder('decrease');
+    } else {
+      self.changeOrder('increase');
+    }
+  }
+
+  object.transition = function() {
+    var self = this;
+    var slides = self.selector.find('.fc-slides');
+
+    if(self.options.transition === 'slide') {
+      slides.toggleClass('fc-slide-animation');
+    }
+  }
+
+  object.transitionClasses = function() {
     var self = this;
 
     if(self.options.transition === 'slide') {
