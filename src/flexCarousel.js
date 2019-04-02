1bd8047e81ef302928475d4dbc570b4a76cab8b8
Elizabeth Makayed <elizabeth.makayed@dnlomnimedia.com>
2019-04-01 17:45:42 -0700

    Added 'slidesPerPage' option to support multiple items per page navigation.

--- src/flexCarousel.js
+++ src/flexCarousel.js
@@ -38,6 +38,7 @@
         nextArrow: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" class="svg-inline--fa fa-angle-right fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg>',
         prevArrow: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left" class="svg-inline--fa fa-angle-left fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>',
         slidesVisible: 1,
+        slidesPerPage: 1,
         transition: 'slide',
       };
 
@@ -103,9 +104,10 @@
     let circle = self.selector.find('.fc-circle');
 
     if(self.options.circles) {
-      circle.click(function() {
-        if(!$(this).hasClass('fc-is-active')) {
-          let index = $(this).index();
+      circle.click(function(event) {
+        let $clickedCircle = $(event.currentTarget);
+        if(!$clickedCircle.hasClass('fc-is-active')) {
+          let index = $clickedCircle.data('fc-slide-index');
           self.goToSlide(index);
         }
       });
@@ -124,8 +126,10 @@
 
         let circles = self.selector.find('.fc-circles');
 
-        slide.each(function () {
-          circles.append('<div class="fc-circle"><span class="fc-icon fc-is-circle"></span></div>');
+        slide.each(function (index) {
+          if (index % self.options.slidesPerPage === 0) {
+            circles.append('<div class="fc-circle" data-fc-slide-index="' + index + '"><span class="fc-icon fc-is-circle"></span></div>');
+          }
         });
 
         let circle = self.selector.find('.fc-circle');
