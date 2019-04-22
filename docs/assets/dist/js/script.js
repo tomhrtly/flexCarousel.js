783a965c7df65f35abf6cc50e14bccdf6c6a206c
Tom Hartley <tom@tomhrtly.com>
2019-04-22 16:10:33 +0100

    Fix dist

---
+++ docs/assets/dist/js/script.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+
+  gtag('js', new Date());
+  gtag('config', 'UA-70809338-11');
+
+}());
+//# sourceMappingURL=script.js.map
