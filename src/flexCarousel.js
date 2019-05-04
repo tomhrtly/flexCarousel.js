c36b1a305e52b460c38f44a1fdd66c3c1648d59d
Tom Hartley <tom@tomhrtly.com>
2019-05-04 12:51:22 +0100

    Add slidesScrolling option

--- src/flexCarousel.js
+++ src/flexCarousel.js
@@ -24,6 +24,7 @@ class FlexCarousel {
       height: null,
       nextArrow: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" class="svg-inline--fa fa-angle-right fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg>',
       prevArrow: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left" class="svg-inline--fa fa-angle-left fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>',
+      slidesScrolling: 1,
       slidesVisible: 1,
       transition: 'slide',
       transitionSpeed: 250
@@ -52,7 +53,6 @@ class FlexCarousel {
   }
 
   addTransition() {
-    console.log('addTransition');
     const self = this;
     const slides = this.selector.querySelector('.fc-slides');
 
