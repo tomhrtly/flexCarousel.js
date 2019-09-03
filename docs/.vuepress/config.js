1868c71327c2e549e073e00caecfb5dcb831295f
Tom Hartley <tom@tomhrtly.com>
2019-09-03 20:49:51 +0100

    Style navbar and add data

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,12 @@
 module.exports = {
     title: 'flexCarousel.js',
-    description: 'A simple, lightweight Flexbox carousel JavaScript module.'
+    description: 'A simple, lightweight Flexbox carousel JavaScript module.',
+    head: [
+        ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Bree+Serif' }]
+    ],
+    themeConfig: {
+        github: 'https://github.com/tomhrtly/flexCarousel.js',
+        twitter: 'https://twitter.com/tomhrtly',
+        download: 'https://github.com/tomhrtly/flexCarousel.js/releases/download/v0.2.1/flexCarousel-0.2.1.zip',
+    }
 };
