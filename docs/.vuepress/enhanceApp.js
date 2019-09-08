cc1ec7105ae1d9917a341139da04f6dbee26db65
Tom Hartley <tom@tomhrtly.com>
2019-09-08 15:10:57 +0100

    Add carousel component

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,4 +1,12 @@
-export default ({ router, siteData }) => {
+import FlexCarousel from '../../src/flexCarousel';
+
+window.FlexCarousel = FlexCarousel;
+
+export default ({
+    Vue,
+    router,
+    siteData
+}) => {
     router.addRoutes([
         { path: '/docs', redirect: `/docs/${siteData.themeConfig.currentVersion}` },
         { path: `/docs/${siteData.themeConfig.currentVersion}`, redirect: `/docs/${siteData.themeConfig.currentVersion}/installation` },
