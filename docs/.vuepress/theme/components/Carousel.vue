026bd2a8b70840f6e5738812e66a9b18d8bd1ae5
Tom Hartley <tom@tomhrtly.com>
2019-09-08 15:10:57 +0100

    Add carousel component

--- docs/.vuepress/theme/components/Carousel.vue
+++ docs/.vuepress/theme/components/Carousel.vue
@@ -5,16 +5,9 @@
 </template>
 
 <script>
-    import FlexCarousel from '../../../../src/flexCarousel';
-
     export default {
-        data() {
-            return {
-                id: this.$attrs.id,
-            }
-        },
-        created() {
-            new FlexCarousel(`#${this.id}`);
+        mounted() {
+            new FlexCarousel(`#${this.$attrs.id}`);
         }
     }
 </script>
