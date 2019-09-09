e5adb8e60066112f633549e1132d12adee08b7a6
Tom Hartley <tom@tomhrtly.com>
2019-09-09 11:37:09 +0100

    Add startingSlide option

--- src/flexCarousel.js
+++ src/flexCarousel.js
@@ -50,16 +50,18 @@ class FlexCarousel {
             prevArrow: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left" class="svg-inline--fa fa-angle-left fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>',
             slidesScrolling: 1,
             slidesVisible: 1,
+            startingSlide: 0,
             transition: 'slide',
             transitionSpeed: 250,
         };
 
+        this.options = extend(this.defaults, options);
+
         this.slideWidth = null;
         this.slideOffset = null;
         this.slideAmount = null;
-        this.currentSlide = 0;
+        this.currentSlide = this.options.startingSlide;
 
-        this.options = extend(this.defaults, options);
         this.init();
     }
 
