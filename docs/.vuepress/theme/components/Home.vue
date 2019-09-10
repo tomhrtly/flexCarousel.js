150aeed78928ff866e2ce75978f543dbc19f08cd
Tom Hartley <tom@tomhrtly.com>
2019-09-10 20:36:07 +0100

    Add target blank

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -86,7 +86,7 @@
                     <h3>Sandbox</h3>
                     <p>Play around with all of the customisation options available for flexCarousel.js by using CodePen, a playground for developers.</p>
                     <figure class="image">
-                        <a href="https://codepen.io/tomhrtly/pen/eXRpOg/">
+                        <a href="https://codepen.io/tomhrtly/pen/eXRpOg/" target="_blank">
                             <svg class="codepen-logo" viewBox="0 0 138 26" width="75%"><path d="M15 8a7 7 0 1 0 0 10m7-8.7L33 2l11 7.3v7.4L33 24l-11-7.3zm0 0l11 7.4 11-7.4m0 7.4L33 9.3l-11 7.4M33 2v7.3m0 7.4V24M52 6h5a7 7 0 0 1 0 14h-5zm28 0h-9v14h9m-9-7h6m11 1h6a4 4 0 0 0 0-8h-6v14m26-14h-9v14h9m-9-7h6m11 7V6l11 14V6"></path></svg>
                         </a>
                     </figure>
