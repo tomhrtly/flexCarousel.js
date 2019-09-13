06fcb9cbf502467c9ea53c2223657e2e0d41f4c7
Tom Hartley <tom@tomhrtly.com>
2019-09-13 16:20:55 +0100

    Add build breakpoints function

--- src/flexCarousel.js
+++ src/flexCarousel.js
@@ -51,6 +51,7 @@ class FlexCarousel {
             infinite: true,
             nextButton: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" class="svg-inline--fa fa-angle-right fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg>',
             prevButton: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left" class="svg-inline--fa fa-angle-left fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>',
+            responsive: null,
             slidesPerPage: 1,
             slidesScrolling: 1,
             transition: 'slide',
@@ -62,6 +63,7 @@ class FlexCarousel {
         this.slideAmount = null;
         this.currentPage = 0;
         this.autoplayDirection = 'right';
+        this.breakpoints = [];
 
         this.options = extend(this.defaults, options);
         this.init();
@@ -170,6 +172,18 @@ class FlexCarousel {
                 this.buildArrowEvents();
                 this.updateArrows();
             }
+        }
+    }
+
+    buildBreakpoints() {
+        if (this.options.responsive) {
+            this.options.responsive.forEach((value, index) => {
+                const { breakpoint } = this.options.responsive[index];
+
+                if (!this.breakpoints.includes(breakpoint)) {
+                    this.breakpoints.push(breakpoint);
+                }
+            });
         }
     }
 
@@ -318,6 +332,7 @@ class FlexCarousel {
             this.buildArrows();
             this.buildCircles();
             this.buildOptions();
+            this.buildBreakpoints();
         }
     }
 
