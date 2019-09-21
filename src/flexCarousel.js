d45ab8235ac516a2ee68f205400407a293662e5e
Tom Hartley <tom@tomhrtly.com>
2019-09-21 20:08:53 +0100

    Remove currentPage 0

--- src/flexCarousel.js
+++ src/flexCarousel.js
@@ -23,6 +23,7 @@ class FlexCarousel {
             circlesOverlay: true,
             height: null,
             infinite: true,
+            initialPage: 0,
             nextButton: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" class="svg-inline--fa fa-angle-right fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg>',
             prevButton: '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-left" class="svg-inline--fa fa-angle-left fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>',
             responsive: null,
@@ -35,11 +36,12 @@ class FlexCarousel {
         this.activeBreakpoint = null;
         this.autoplayDirection = 'right';
         this.breakpoints = [];
-        this.currentPage = 0;
         this.options = FlexCarousel.extend(this.defaults, options);
         this.originalOptions = this.options;
         this.slideAmount = null;
         this.slideWidth = null;
+
+        this.currentPage = this.options.initialPage;
 
         this.init();
     }
@@ -320,8 +322,6 @@ class FlexCarousel {
 
         this.selector.className = this.selectorName.replace('.', '');
         this.selector.removeAttribute('style');
-
-        this.currentPage = 0;
     }
 
     getLeftPage(index) {
