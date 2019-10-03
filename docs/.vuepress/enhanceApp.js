e63caf0409748663f12f8db25afe55f8551786c1
Tom Hartley <tom@tomhrtly.com>
2019-10-03 20:22:42 +0100

    Fix window is not defined

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -12,7 +12,8 @@ export default ({
     Vue.use(VuePrism);
     router.addRoutes([
         { path: '/docs', redirect: `/docs/${siteData.themeConfig.currentVersion}` },
-        { path: `/docs/${siteData.themeConfig.currentVersion}`, redirect: `/docs/${siteData.themeConfig.currentVersion}/installation` },
+        { path: `/docs/master/`, redirect: `/docs/master/installation` },
+        { path: `/docs/1.0.0/`, redirect: `/docs/1.0.0/installation` },
         { path: '/examples', redirect: '/examples/basic' },
     ])
 }
