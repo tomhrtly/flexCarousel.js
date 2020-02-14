0c13b73489af3ff4376bab50a0df9698f77313aa
Tom Hartley <tom@tomhrtly.com>
2020-02-14 11:14:43 +0000

    Add set function

--- src/components/swipe.js
+++ src/components/swipe.js
@@ -1,20 +1,50 @@
-function start(fc, event) {
+function set(fc, event) {
     let touches;
+
+    if (event.touches && event.touches.length) {
+        touches = event.touches;
+    }
+
+    fc._touch.curX = touches ? touches[0].pageX : event.clientX;
+    fc._touch.curY = touches ? touches[0].pageY : event.clientY;
+}
+
+function start(fc, event) {
+    set(fc, event);
+
+    fc._touch.startX = fc._touch.curX;
+    fc._touch.startY = fc._touch.curY;
 
     if (fc._touch.fingers !== 1 || fc._pageAmount <= fc._options.slidesPerPage) {
         fc._touch = {};
         return false;
     }
 
-    if (event.touches) {
-        touches = event.touches.length;
+    fc._dragging = true;
+    return true;
+}
+
+function move(fc, event) {
+    if (!fc._dragging) {
+        return false;
     }
 
-    fc._touch.startX = touches !== undefined ? touches.pageX : event.clientX;
-    fc._touch.startY = touches !== undefined ? touches.pageY : event.clientY;
-    fc._dragging = true;
+    set(fc, event);
 
-    return true;
+    fc._touch.swipeLength = Math.round(Math.sqrt((fc._touch.curX - fc._touch.startX) ** 2));
+
+    if (!fc._swiping) {
+        fc._scrolling = true;
+        return false;
+    }
+
+    if (fc._touch.swipeLength > 4) {
+        fc._swiping = true;
+        event.preventDefault();
+    }
+
+    const positionOffset = (fc._touch.curX > fc._touch.startX ? 1 : -1);
+    fc._touch.edgeHit = false;
 }
 
 function controller(fc, event) {
@@ -23,6 +53,8 @@ function controller(fc, event) {
 
     if (event.type === 'touchstart' || event.type === 'mousedown') {
         start(fc, event);
+    } else if (event.type === 'touchmove' || event.type === 'mousemove') {
+        move(fc, event);
     }
 }
 
