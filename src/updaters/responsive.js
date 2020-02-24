7ab365724ee696837b2f48471e8f399ff0daa6f1
Tom Hartley <tom@tomhrtly.com>
2020-02-24 16:35:15 +0000

    Remove file

--- src/updaters/responsive.js
+++ src/updaters/responsive.js
@@ -1,11 +1,33 @@
-import destroy from '../core/destroy';
 import extend from '../utils/extend';
 
-function reinit(options = {}) {
-    destroy(this);
-    this._options = extend(this._defaults, options);
-    this._init();
-    this._selector.dispatchEvent(this._events.breakpoint);
+function destroy(fc) {
+    fc._selector.querySelectorAll('.fc-slide.fc-is-clone').forEach((element) => {
+        fc._selector.querySelector('.fc-slides').removeChild(element);
+    });
+
+    fc._selector.querySelectorAll('.fc-slide').forEach((element) => {
+        element.removeAttribute('class');
+        element.removeAttribute('style');
+    });
+
+    fc._selector.querySelector('.fc-slides').removeAttribute('style');
+    fc._selector.querySelector('.fc-slides').removeAttribute('class');
+
+    if (fc._options.circles) {
+        fc._selector.querySelector('.fc-container').removeChild(fc._selector.querySelector('.fc-circles'));
+    }
+
+    fc._selector.innerHTML = fc._selector.querySelector('.fc-container').innerHTML;
+
+    fc._selector.className = fc._selectorName.replace('.', '');
+    fc._selector.removeAttribute('style');
+}
+
+function reinit(fc, options = {}) {
+    destroy(fc);
+    fc._options = extend(fc._defaults, options);
+    fc._init();
+    fc._selector.dispatchEvent(fc._events.breakpoint);
 }
 
 export default function (fc) {
@@ -23,14 +45,14 @@ export default function (fc) {
         if (fc._activeBreakpoint) {
             if (targetBreakpoint !== fc._activeBreakpoint) {
                 fc._activeBreakpoint = targetBreakpoint;
-                reinit(fc._breakpoints[targetBreakpoint]);
+                reinit(fc, fc._breakpoints[targetBreakpoint]);
             }
         } else {
             fc._activeBreakpoint = targetBreakpoint;
-            reinit(fc._breakpoints[targetBreakpoint]);
+            reinit(fc, fc._breakpoints[targetBreakpoint]);
         }
     } else if (fc._activeBreakpoint !== null) {
         fc._activeBreakpoint = null;
-        reinit(original);
+        reinit(fc, original);
     }
 }
