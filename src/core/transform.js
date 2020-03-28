1ef1c989166c1f81c3e8fc6f67dcd500e2d9fdee
Tom Hartley <tom@tomhrtly.com>
2020-03-28 10:28:07 +0000

    Add percentage width

--- src/core/transform.js
+++ src/core/transform.js
@@ -1,3 +1,3 @@
 export default function (fc, position) {
-    fc._selector.querySelector('.fc-slides').style.transform = `translate3d(${Math.ceil(position)}px, 0px, 0px)`;
+    fc._selector.querySelector('.fc-slides').style.transform = `translate3d(${Math.ceil(position)}%, 0px, 0px)`;
 }
