d6fc27642138b8d5d1bd9b94936a4eaf970813ed
Tom Hartley <tom@tomhrtly.com>
2020-08-25 11:53:19 +0100

    Disable rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     ],
     rules: {
         'class-methods-use-this': 0,
+        'import/no-named-as-default': 0,
         'indent': ['error', 4],
         'max-len': 0,
         'no-param-reassign': [2, { 'props': false }],
